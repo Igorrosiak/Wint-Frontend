6861b4a5fba5fd18d4f690285666e5cf2c3d1f9e
Igor Luiz Belter Rosiak <igorluizbelterrosiak@gmail.com>
2023-08-18 16:36:54 -0300

    sass-to-tailwind-responsive

--- src/pages/signUp/signUp.tsx
+++ src/pages/signUp/signUp.tsx
@@ -6,12 +6,12 @@ const SignUp = () => {
   const [activeInput, setActiveInput] = useState<boolean>(false);
 
   return (
-    <div className="sign-up-page w-[100vw] h-[100vh] overflow-hidden flex items-center justify-between bg-[#222222] text-white">
-      <aside className="aside-logo w-1/2">
+    <div className="sign-up-page w-[100vw] h-[100vh] overflow-hidden flex items-center justify-between bg-[#222222] text-white max-[992px]:flex-col max-[992px]:justify-center">
+      <aside className="aside-logo w-1/2 max-[992px]:w-[200px] max-[992px]:mb-4 max-[320px]:w-[150px]">
         <img src={wintLogo} alt="Wint Logotype" className="ml-auto" />
       </aside>
-      <aside className="aside-form w-2/5 mx-auto">
-        <h1 className="mb-3 text-5xl text-center font-normal">Primeira vez?</h1>
+      <aside className="aside-form w-2/5 mx-auto max-[992px]:w-[90vw]">
+        <h1 className="mb-3 text-5xl text-center font-normal max-[450px]:text-4xl">Primeira vez?</h1>
         <p className="mb-7 text-lg text-center text-gray-400">
           Preencha os dados abaixo para realizar seu cadastro.
         </p>
