2c382f7dc344fd2d38c81a3dfc0091b64883407a
Igor Luiz Belter Rosiak <igorluizbelterrosiak@gmail.com>
2023-08-22 11:27:36 -0300

    hover-icons-heroBanner

--- src/components/profile/heroBanner/heroBanner.tsx
+++ src/components/profile/heroBanner/heroBanner.tsx
@@ -9,14 +9,14 @@ const HeroBanner = () => {
         <h1 className="my-4 text-5xl font-bold">Gabriela Senna</h1>
         <p className="py-4">Seja bem vindo ao meu portfolio virtual da Wint, aqui você encotrara meus trabalhos do senai, cursos especializantes e demais áreas que mando bem!</p>
         <section className="flex gap-4 text-[#4d3a99] max-[768px]:justify-center">
-            <BsInstagram size={35} className="cursor-pointer" />
-            <BsGithub size={35} className="cursor-pointer" />
-            <BsTwitter size={35} className="cursor-pointer" />
-            <BsLinkedin size={35} className="cursor-pointer" />
+            <BsInstagram size={35} className="cursor-pointer hover:scale-110 transition-all" />
+            <BsGithub size={35} className="cursor-pointer hover:scale-110 transition-all" />
+            <BsTwitter size={35} className="cursor-pointer hover:scale-110 transition-all" />
+            <BsLinkedin size={35} className="cursor-pointer hover:scale-110 transition-all" />
         </section>
       </aside>
       <aside className="w-1/2 max-[768px]:w-full">
-        <img src={profileImage} alt="Profile Image" className="m-auto max-[768px]:w-[350px]" />
+        <img src={profileImage} alt="Profile User" className="m-auto max-[768px]:w-[350px]" />
       </aside>
     </section>
   );
