0487a3e40cdb79e6f7ab63db637c65d3efe4e358
Igor Luiz Belter Rosiak <igorluizbelterrosiak@gmail.com>
2023-08-22 23:56:47 -0300

    professionalExperience-turn-works

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,8 +1,8 @@
 import Header from "../../components/header/header";
 import HeroBanner from "../../components/profile/heroBanner/heroBanner";
 import Skills from "../../components/profile/skills/skills";
+import Curriculum from "../../components/profile/curriculum/curriculum";
 import Works from "../../components/profile/works/works";
-import ProfessionalExperience from "../../components/profile/professionalExperiences/professionalExperiences";
 import Blog from "../../components/profile/blogs/blogs";
 import Recommendations from "../../components/profile/recommendations/recommendations";
 import Footer from "../../components/footer/footer";
@@ -13,8 +13,8 @@ const Profile = () => {
       <Header />
       <HeroBanner />
       <Skills />
+      <Curriculum />
       <Works />
-      <ProfessionalExperience />
       <Blog />
       <Recommendations />
       <Footer />
