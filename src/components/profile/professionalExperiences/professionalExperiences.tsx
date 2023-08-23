a337c4b91b3db49f7a4f24a6616c4c7c4f148ff7
Igor Luiz Belter Rosiak <igorluizbelterrosiak@gmail.com>
2023-08-22 23:56:20 -0300

    professionalExperience-turn-works

--- src/components/profile/professionalExperiences/professionalExperiences.tsx
+++
@@ -1,7 +0,0 @@
-import { useState, useEffect } from "react";
-
-const ProfessionalExperiences = () => {
-    return <section id="professionalExperience"></section>
-};
-
-export default ProfessionalExperiences;
