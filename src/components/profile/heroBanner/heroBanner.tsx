608254fd902a8db621ff2dcf7aa6fdd484a0b269
igorrosiak <igorluizbelterrosiak@gmail.com>
2023-09-02 14:04:20 -0300

    user-data-on-heroBanner

--- src/components/profile/heroBanner/heroBanner.tsx
+++ src/components/profile/heroBanner/heroBanner.tsx
@@ -1,19 +1,30 @@
 import { BsInstagram, BsGithub, BsTwitter, BsLinkedin } from "react-icons/bs"
-import { PropsId } from "../../../types/propsId";
+import { UserResponse } from "../../../types/user";
 import profileImage from "../../../assets/profile-image.png"
 
-const HeroBanner = ({userId}: PropsId) => {
+interface HeroBannerProps {
+  user: UserResponse
+}
+
+const HeroBanner = ({user}: HeroBannerProps) => {
   return (
     <section className="h-[90vh] min-[768px]:pb-32 flex justify-center items-center max-[768px]:flex-col-reverse">
       <aside className="w-1/3 max-[768px]:w-[85vw] max-[768px]:text-center m-auto">
-        <h3 className="text-gray-400">- H E L L O W O R L D</h3>
-        <h1 className="my-4 text-5xl font-bold max-md:text-4xl">Gabriela Senna</h1>
-        <p className="py-4">Seja bem vindo ao meu portfolio virtual da Wint, aqui você encotrará meus trabalhos do senai, cursos especializantes e demais áreas que mando bem!</p>
+        <h1 className="my-4 text-5xl font-bold max-md:text-4xl">{user.name}</h1>
+        <p className="py-4">{user.presentation ? user.presentation : 'Seja bem-vindo ao meu perfil da Wint!'}</p>
         <section className="flex gap-4 text-[#4d3a99] max-[768px]:justify-center">
-            <BsInstagram size={35} className="cursor-pointer hover:scale-110 transition-all" />
-            <BsGithub size={35} className="cursor-pointer hover:scale-110 transition-all" />
-            <BsTwitter size={35} className="cursor-pointer hover:scale-110 transition-all" />
-            <BsLinkedin size={35} className="cursor-pointer hover:scale-110 transition-all" />
+          {user.instagramLink && (
+            <BsInstagram size={35} className="cursor-pointer hover:scale-110 transition-all" onClick={() => window.open(user.instagramLink)} />
+          )}
+          {user.githubLink && (
+            <BsGithub size={35} className="cursor-pointer hover:scale-110 transition-all" onClick={() => window.open(user.githubLink)} />
+          )}
+          {user.twitterLink && (
+            <BsTwitter size={35} className="cursor-pointer hover:scale-110 transition-all" onClick={() => window.open(user.twitterLink)} />
+          )}
+          {user.linkedinLink && (
+            <BsLinkedin size={35} className="cursor-pointer hover:scale-110 transition-all" onClick={() => window.open(user.linkedinLink)} />
+          )}
         </section>
       </aside>
       <aside className="w-1/2 max-[768px]:w-full">
