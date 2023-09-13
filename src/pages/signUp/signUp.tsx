9fd702ca635e4235d93d532a72a76c2722577abe
igorrosiak <igorluizbelterrosiak@gmail.com>
2023-09-13 15:56:50 -0300

    handleNewUser-working

--- src/pages/signUp/signUp.tsx
+++ src/pages/signUp/signUp.tsx
@@ -1,9 +1,52 @@
 import { useState } from "react";
+import Swal from "sweetalert2";
+import moment from "moment";
+import postUser from "../../services/user/postUser";
+import { UserResponse } from "../../types/user";
 import wintLogo from "../../assets/wintLogo.png";
-import "./signUp.scss"
+import "./signUp.scss";
 
 const SignUp = () => {
+  const [name, setName] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [birthDate, setBirthDate] = useState<string>("");
+
   const [activeInput, setActiveInput] = useState<boolean>(false);
+
+  async function handleNewUser() {
+    if (!name || !username || !email || !password || !birthDate) {
+      return Swal.fire(
+        "Erro",
+        "Por favor, preencha todos os campos corretamente.",
+        "error"
+      );
+    }
+    
+    const userCreated = await postUser({
+      name,
+      username,
+      email,
+      password,
+      birthDate: moment(birthDate).format("DD/MM/YYYY"),
+      role: "USER",
+    });
+
+    if(userCreated.id){
+      return Swal.fire(
+        "Sucesso",
+        "Parabéns, seu usuário foi criado com sucesso.",
+        "success"
+      );
+    } else {
+      return Swal.fire(
+        "Erro",
+        "Desculpe, algo deu errado ao criar seu usuário... <br /> Confira os dados inseridos e tente novamente.",
+        "error"
+      );
+    }
+  }
 
   return (
     <div className="sign-up-page w-[100vw] h-[100vh] overflow-hidden flex items-center justify-between bg-[#222222] text-white max-[992px]:flex-col max-[992px]:justify-center">
@@ -11,7 +54,9 @@ const SignUp = () => {
         <img src={wintLogo} alt="Wint Logotype" className="ml-auto" />
       </aside>
       <aside className="aside-form w-2/5 mx-auto max-[992px]:w-[90vw]">
-        <h1 className="mb-3 text-5xl text-center font-normal max-[450px]:text-4xl">Primeira vez?</h1>
+        <h1 className="mb-3 text-5xl text-center font-normal max-[450px]:text-4xl">
+          Primeira vez?
+        </h1>
         <p className="mb-7 text-lg text-center text-gray-400">
           Preencha os dados abaixo para realizar seu cadastro.
         </p>
@@ -22,7 +67,18 @@ const SignUp = () => {
           <input
             type="text"
             id="name"
-            placeholder="Meu Nome Completo"
+            placeholder="Insira seu nome completo"
+            onChange={(e) => setName(e.target.value)}
+            className="bg-[#383838] h-12 rounded-lg indent-3 mb-3"
+          />
+          <label htmlFor="username" className="font-semibold text-lg mb-1">
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Insira seu username"
+            onChange={(e) => setUsername(e.target.value)}
             className="bg-[#383838] h-12 rounded-lg indent-3 mb-3"
           />
           <label htmlFor="date" className="font-semibold text-lg mb-1">
@@ -34,7 +90,10 @@ const SignUp = () => {
             className={`bg-[#383838] h-12 rounded-lg indent-1 pl-1 pr-4 mb-3 ${
               !activeInput && "text-gray-400"
             }`}
-            onChange={(e) => setActiveInput(true)}
+            onChange={(e) => {
+              setBirthDate(e.target.value);
+              setActiveInput(true);
+            }}
           />
           <label htmlFor="email" className="font-semibold text-lg mb-1">
             Email
@@ -43,6 +102,7 @@ const SignUp = () => {
             type="email"
             id="email"
             placeholder="name@company.com"
+            onChange={(e) => setEmail(e.target.value)}
             className="bg-[#383838] h-12 rounded-lg indent-3 mb-3"
           />
           <label htmlFor="password" className="font-semibold text-lg mb-1">
@@ -52,9 +112,14 @@ const SignUp = () => {
             type="password"
             id="password"
             placeholder="********"
+            onChange={(e) => setPassword(e.target.value)}
             className="bg-[#383838] h-12 rounded-lg indent-3 mb-4"
           />
-          <button className="bg-[#652EBE] hover:bg-[#5724a7] transition-all h-14 rounded-xl font-semibold text-xl">
+          <button
+            type="button"
+            onClick={() => handleNewUser()}
+            className="bg-[#652EBE] hover:bg-[#5724a7] transition-all h-14 rounded-xl font-semibold text-xl"
+          >
             Cadastrar-se
           </button>
           <p className="mt-4 text-center">
--- src/services/user/postUser.ts
+++ src/services/user/postUser.ts
@@ -2,12 +2,12 @@ import { api } from '../../config/axios';
 import { UserRequest, UserResponse } from '../../types/user';
 
 export default function postUser(body: UserRequest): Promise<UserResponse> {
-  return api.post(`user`, body).then(
+  return api.post(`user/`, body).then(
     (response) => {
       return response.data;
     },
     (error) => {
-      return error.response.status;
+      return error;
     }
   );
 }
