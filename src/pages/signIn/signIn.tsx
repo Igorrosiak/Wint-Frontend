b650c815bf9fa426a402f724bc30a15969c66abe
unknown <igorluizbelterrosiak@gmail.com>
2025-03-05 19:21:05 -0300

    :sparkles: feat: user login working

--- src/pages/signIn/signIn.tsx
+++ src/pages/signIn/signIn.tsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import Swal from "sweetalert2";
 import postLogin from "../../services/auth/postLogin";
 import wintLogo from "../../assets/wintLogo.png";
+import { useCookies } from "react-cookie";
 
 const SignIn = () => {
+  const [cookies, setCookie] = useCookies(["token"]);
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
@@ -24,15 +26,18 @@ const SignIn = () => {
         "error"
       );
     }
-    
+
     const userLogged = await postLogin({
       email,
       password,
     });
 
-    if(userLogged.token){
-      console.log(userLogged.token)
-      // window.location.assign("/");
+    if (userLogged.accessToken && userLogged.refreshToken && userLogged.user) {
+      localStorage.setItem("refreshToken", userLogged.refreshToken);
+      localStorage.setItem("username", userLogged.user.username);
+      localStorage.setItem("userLogged", "true");
+      window.location.assign(`/profile/${userLogged.user.username}`);
+      // setCookie('token', userLogged.accessToken, { path: '/' });
     } else {
       return Swal.fire(
         "Erro",
@@ -45,12 +50,17 @@ const SignIn = () => {
   return (
     <div className="sign-in-page w-[100vw] h-[100vh] overflow-hidden flex items-center justify-between bg-[#222222] text-white max-[992px]:flex-col-reverse max-[992px]:justify-center">
       <aside className="aside-form w-2/5 mx-auto max-[992px]:w-[80vw]">
-        <h1 className="mb-3 text-6xl text-center font-normal max-[992px]:text-4xl min-[2000px]:text-9xl">Bem-vindo(a)!</h1>
+        <h1 className="mb-3 text-6xl text-center font-normal max-[992px]:text-4xl min-[2000px]:text-9xl">
+          Bem-vindo(a)!
+        </h1>
         <p className="mb-7 text-xl text-center text-gray-400 max-[992px]:text-lg min-[2000px]:text-3xl">
           Insira suas credenciais abaixo para conectar-se.
         </p>
         <form className="flex flex-col max-w-xl m-auto min-[2000px]:max-w-none">
-          <label htmlFor="email" className="font-semibold text-lg mb-1 min-[2000px]:text-3xl">
+          <label
+            htmlFor="email"
+            className="font-semibold text-lg mb-1 min-[2000px]:text-3xl"
+          >
             Email
           </label>
           <input
@@ -60,7 +70,10 @@ const SignIn = () => {
             onChange={(e) => setEmail(e.target.value)}
             className="bg-[#383838] h-12 rounded-lg indent-3 mb-3 max-[992px]:h-10 min-[2000px]:h-16"
           />
-          <label htmlFor="password" className="font-semibold text-lg mb-1 min-[2000px]:text-3xl">
+          <label
+            htmlFor="password"
+            className="font-semibold text-lg mb-1 min-[2000px]:text-3xl"
+          >
             Senha
           </label>
           <input
@@ -70,10 +83,11 @@ const SignIn = () => {
             onChange={(e) => setPassword(e.target.value)}
             className="bg-[#383838] h-12 rounded-lg indent-3 mb-3 max-[992px]:h-10 min-[2000px]:h-16"
           />
-          <button 
-            type="button" 
+          <button
+            type="button"
             onClick={() => handleLogin()}
-            className="bg-[#652EBE] hover:bg-[#5724a7] h-14 mt-2 rounded-xl font-semibold text-xl min-[2000px]:h-20 min-[2000px]:text-3xl">
+            className="bg-[#652EBE] hover:bg-[#5724a7] h-14 mt-2 rounded-xl font-semibold text-xl min-[2000px]:h-20 min-[2000px]:text-3xl"
+          >
             Login
           </button>
           <p className="mt-4 text-center min-[2000px]:text-2xl">
