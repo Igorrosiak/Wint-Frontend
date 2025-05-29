166cddb5b1702460468a5f28a20d728fba998fc0
Igor Luiz <igorluizbelterrosiak@gmail.com>
2025-05-29 18:43:58 -0300

    :ok_hand: refactor: refactoring footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,26 +1,68 @@
-import { BsInstagram, BsFacebook, BsGithub, BsTwitter, BsLinkedin } from "react-icons/bs";
+import {
+  BsInstagram,
+  BsFacebook,
+  BsGithub,
+  BsTwitter,
+  BsLinkedin,
+} from "react-icons/bs";
 
-const Footer = () => {
+interface FooterProps {
+  linkedinLink?: string;
+  githubLink?: string;
+  instagramLink?: string;
+  facebookLink?: string;
+  twitterLink?: string;
+}
+
+const Footer = ({
+  linkedinLink,
+  githubLink,
+  instagramLink,
+  facebookLink,
+  twitterLink,
+}: FooterProps) => {
   return (
     <footer className="py-10 px-4 text-[#4d3a99]">
       <div className="row-social-media flex gap-4 items-center justify-center">
-        <BsInstagram
-          size={28}
-          className="cursor-pointer hover:scale-110 transition-all"
-          onClick={() => window.open("https://www.instagram.com/igor.rosiak/")}
-        />
-        <BsGithub
-          size={28}
-          className="cursor-pointer hover:scale-110 transition-all"
-          onClick={() => window.open("https://www.github.com/igorrosiak")}
-        />
-        <BsLinkedin
-          size={28}
-          className="cursor-pointer hover:scale-110 transition-all"
-          onClick={() => window.open("https://www.linkedin.com/in/igor-rosiak/")}
-        />
+        {linkedinLink && (
+          <BsLinkedin
+            size={28}
+            className="cursor-pointer hover:scale-110 transition-all"
+            onClick={() => window.open(linkedinLink)}
+          />
+        )}
+        {githubLink && (
+          <BsGithub
+            size={28}
+            className="cursor-pointer hover:scale-110 transition-all"
+            onClick={() => window.open(githubLink)}
+          />
+        )}
+        {instagramLink && (
+          <BsInstagram
+            size={28}
+            className="cursor-pointer hover:scale-110 transition-all"
+            onClick={() => window.open(instagramLink)}
+          />
+        )}
+        {facebookLink && (
+          <BsFacebook
+            size={28}
+            className="cursor-pointer hover:scale-110 transition-all"
+            onClick={() => window.open(facebookLink)}
+          />
+        )}
+        {twitterLink && (
+          <BsTwitter
+            size={28}
+            className="cursor-pointer hover:scale-110 transition-all"
+            onClick={() => window.open(twitterLink)}
+          />
+        )}
       </div>
-      <p className="text-center pt-4">© 2023 - Wint, crie já seu currículo interativo</p>
+      <p className="text-center pt-4">
+        © 2023 - Wint, crie já seu currículo interativo
+      </p>
     </footer>
   );
 };
